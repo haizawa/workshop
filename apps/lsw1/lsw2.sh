f6c49b0ea1fe34328822a2a14bd330ab4bc86bd3
Hajime Aizawa <aizawa.github@gmail.com>
2015-03-21 13:41:41 +0900

    lsw2

---
+++ apps/lsw1/lsw2.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+## remove proxy
+unset http_proxy
+
+FORMAT="\n%{url_effective}, %{response_code}\n\n"
+ADDRESS=0.0.0.0
+
+#sleep 1
+# view ComponentManagers
+curl http://$ADDRESS:10080/systemmanager/component_managers | python -mjson.tool
+
+# create Components
+curl -w "$FORMAT" http://$ADDRESS:10080/systemmanager/components/network0 -X PUT -d \
+'{"id": "network1", "type": "Network"}'
+curl -w "$FORMAT" http://$ADDRESS:10080/systemmanager/components/network1 -X PUT -d \
+'{"id": "network1", "type": "Network"}'
+curl -w "$FORMAT" http://$ADDRESS:10080/systemmanager/components/learning_sw -X PUT -d \
+'{"id": "learning_sw", "type": "LearningSwitch"}'
+curl -w "$FORMAT" http://$ADDRESS:10080/systemmanager/components/aggregator -X PUT -d \
+'{"id": "aggregator", "type": "Aggregator"}'
+curl -w "$FORMAT" http://$ADDRESS:10080/systemmanager/components/openflow_driver -X PUT -d \
+'{"id": "openflow_driver", "type": "OpenFlowDriver"}'
+
+# connect Components
+curl -w "$FORMAT" http://$ADDRESS:10080/systemmanager/connections/conn1 -X PUT -d \
+'{"id": "conn1", "type": "LogicAndNetwork", "connection_type": "original", "logic_id": "learning_sw", "network_id":"network1"}'
+curl -w "$FORMAT" http://$ADDRESS:10080/systemmanager/connections/conn2 -X PUT -d \
+'{"id": "conn2", "type": "LogicAndNetwork", "connection_type": "original", "logic_id": "openflow_driver", "network_id":"network0"}'
+curl -w "$FORMAT" http://$ADDRESS:10080/systemmanager/connections/conn3 -X PUT -d \
+'{"id": "conn3", "type": "LogicAndNetwork", "connection_type": "aggregated", "logic_id": "aggregator", "network_id":"network1"}'
+curl -w "$FORMAT" http://$ADDRESS:10080/systemmanager/connections/conn4 -X PUT -d \
+'{"id": "conn4", "type": "LogicAndNetwork", "connection_type": "original", "logic_id": "aggregator", "network_id":"network0"}'
+curl http://$ADDRESS:10080/systemmanager/connections | python -mjson.tool
+
+# view Components
+curl http://$ADDRESS:10080/systemmanager/components | python -mjson.tool
